197641c09cd59c7c7df04e707638ef7c5d1c514e
Kaustubh Bhalerao <bhalerao.1@gmail.com>
2022-10-17 21:18:54 -0500

    Update admin.py

--- zinnia_ckeditor/admin.py
+++ zinnia_ckeditor/admin.py
@@ -2,7 +2,7 @@
 from django import forms
 from django.contrib import admin
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from zinnia.models import Entry
 from zinnia.admin.entry import EntryAdmin
